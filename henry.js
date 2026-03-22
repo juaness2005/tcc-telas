3f4c39b93d0d17e819ca2bf96927875cceffe238
Juan Gabriel Neves dos Santos <juangabriel4362767@gmail.com>
2026-03-22 01:42:45 -0300

    Add files via upload

--- henry.js
+++ henry.js
@@ -9,11 +9,25 @@ let coracoesClicados = 0;
 
 // 💌 suas mensagens
 const mensagens = [
-  "Você é especial ❤️",
-  "Pensei em você hoje 💭",
-  "Você me faz sorrir 😊",
-  "Te escolheria mil vezes 💖",
-  "Você ilumina meu dia ✨"
+  "Eu escolhi passar pelo ciclone com você",
+  "As vezes me pergunto se vou estragar as coisas entre nós, mas eu poderia literalmente morrer caso isso acontecesse",
+  "Você meio que é o meu sonho americano",
+  "Nós já ficamos juntos na minha cabeça, se é de mentira, então porque parece um juramento ao que nós dois nos submetemos?",
+  "E se o jeito que você me abraçar for o que é sagrado?",
+  "Eles não sabem, mas você tomou conta dos meus pensamentos (acho que nem você sabe)",
+  "Eu escolho eu e você, religiosamente",
+  "Vou te mostrar o paraíso se você for um anjo, só meu",
+  "Por um tempo achei que era melhor estar seguro do que apaixonado, no entanto te conheci e a minha segurança falhou",
+  "Tem uma placa em meu coração e ela diz que ele está reservado a você",
+  "Essa cidade é false, mas você é a coisa real",
+  "Eu posso ser a vida que você tanto deseja",
+  "Eu odeio isso daqui, então vou para um lugar em minha mente onde estamos juntos",
+  "E se os meus olhos se voltassem para cima e encontrassem os seus mais uma vez?",
+  "Por favor, tenho estado de joelhos, mude a profecia. Não quero dinheiro, só alguem que queira a minha companhia",
+  "Não sou doador, mas te daria o meu coração caso você precisasse",
+  "Garfos, facas, pratos, porcelanas, jogos americanos, luminárias, quando conheci você, tudo ficou mais bonito",
+  "Tá tão fácil de encaixar os teus cénarios na minha rotina",
+  "Faz parte da minha rotina te querer"
 ];
 
 // botão fechar popup
